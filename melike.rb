6cf962914db5970e36aa4faae3f4e1b6a1cabcf5
Rajesh Kumar <rajesh@kosmix.com>
2011-01-21 18:42:41 -0800

    checking why it stopped working

--- melike.rb
+++ melike.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require 'erb'
+require 'graph_api_methods'
 
 get '/' do
  File.read(File.join('public', 'index.html'))
+end
+
+get '/friends' do
+  erb :friends, :locals => { :friends => get_friends}
+end
+
+get '/likes' do
+  erb :friends, :locals => { :friends => get_likes}
 end
