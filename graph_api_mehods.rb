b919ed1ce1a26f3ebfdb502641ab5998c5e16584
Rajesh Kumar <rajesh@kosmix.com>
2011-01-21 18:45:04 -0800

    adding some templates

---
+++ graph_api_mehods.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'koala'
+
+class GraphApiMethods
+  def initialize
+    @graph = Koala::Facebook::GraphAPI.new
+  end
+
+  def get_friends
+    
+  end
+
+  def get_likes
+  end
+end
