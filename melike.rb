d12f10fe0f00dead7e762bbe4bf0af14a024d4f2
Rajesh Kumar <rajesh@kosmix.com>
2011-01-23 01:26:34 -0800

    change get to post

--- melike.rb
+++ melike.rb
@@ -11,7 +11,7 @@ get '/friends' do
   erb :friends, :locals => { :friends => GraphApiMethods.new(request.cookies).get_friends}
 end
 
-get '/likes' do
+post '/likes' do
   friend_id = params[:friend]
   erb :likes, :locals => { :likes => GraphApiMethods.new(request.cookies).get_likes(friend_id)}
 end
