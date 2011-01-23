135e6d6321a1a9dec380d96814e8e7954daf7381
Rajesh Kumar <rajesh@kosmix.com>
2011-01-23 00:50:43 -0800

    string id and key

--- graph_api_mehods.rb
+++ graph_api_mehods.rb
@@ -4,7 +4,7 @@ require 'koala'
 class GraphApiMethods
   def initialize(cookies)
     @graph     = Koala::Facebook::GraphAPI.new
-    @oauth     = Koala::Facebook::OAuth.new(105020132906712, 962612f7efff4c587629a7c800453c06)
+    @oauth     = Koala::Facebook::OAuth.new('105020132906712', '962612f7efff4c587629a7c800453c06')
     @user_info = @oauth.get_user_info_from_cookies(cookies)
     @graph     = Koala::Facebook::GraphAPI.new(@user_info['access_token'])
   end
