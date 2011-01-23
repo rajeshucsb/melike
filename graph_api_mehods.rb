c7b94b0d865cc65dd3ad14c195110a304f233827
Rajesh Kumar <rajesh@kosmix.com>
2011-01-23 00:48:57 -0800

    Accessing friends

--- graph_api_mehods.rb
+++ graph_api_mehods.rb
@@ -2,13 +2,15 @@ require 'rubygems'
 require 'koala'
 
 class GraphApiMethods
-  def initialize(session)
-    @graph = Koala::Facebook::GraphAPI.new
-    @session = session
+  def initialize(cookies)
+    @graph     = Koala::Facebook::GraphAPI.new
+    @oauth     = Koala::Facebook::OAuth.new(105020132906712, 962612f7efff4c587629a7c800453c06)
+    @user_info = @oauth.get_user_info_from_cookies(cookies)
+    @graph     = Koala::Facebook::GraphAPI.new(@user_info['access_token'])
   end
 
   def get_friends
-    return @session
+    graph.get_connections("me", "friends")
   end
 
   def get_likes
--- melike.rb
+++ melike.rb
@@ -3,16 +3,14 @@ require 'sinatra'
 require 'erb'
 require File.join(File.dirname(__FILE__), 'graph_api_mehods')
 
-enable :sessions
-
 get '/' do
  File.read(File.join('public', 'index.html'))
 end
 
 get '/friends' do
-  erb :friends, :locals => { :friends => GraphApiMethods.new(session).get_friends}
+  erb :friends, :locals => { :friends => GraphApiMethods.new(request.cookies).get_friends}
 end
 
 get '/likes' do
-  erb :friends, :locals => { :friends => GraphApiMethods.new(session).get_likes}
+  erb :friends, :locals => { :friends => GraphApiMethods.new(request.cookies).get_likes}
 end
